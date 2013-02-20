463698efb99b8b5805d9f20d63194ec0b864f06c
oskarnylen <oskarnylen@gmail.com>
2013-02-20 16:04:13 +0100

    Added class YahooParser that parses directly from Yahoo's website. Contains a finished method to parse data from the balance sheet and an uncomplete method to parse data from the general information about a stock

---
+++ DataCollector/src/collector/YahooParser.java
@@ -0,0 +1,92 @@
+package collector;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * A parser class containing methods to parse data from Yahoo's website.
+ * 
+ * @author oskarnylen
+ */
+
+public class YahooParser {
+	
+	private static double value;
+	
+	/**
+	 * Parses data from the balance sheet from the latest quarterly report.
+	 * 
+	 * @param symbol - the symbol of the company
+	 * @param key - the key of the value eg: "Total Assets" or "Total Stockholder Equity"
+	 * @return
+	 * @throws IOException
+	 */
+	public static double balanceParser(String symbol, String key) throws IOException{
+		
+        URL oracle = new URL("http://finance.yahoo.com/q/bs?s=" + symbol);
+        URLConnection yc = oracle.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                                    yc.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null){
+        	
+        	if(inputLine.contains(key)){
+        		in.mark(8);
+        		in.reset();
+        		while((inputLine = in.readLine()) != null){
+        			
+        			if(inputLine.matches(".*\\d.*")){
+        				
+        				String[] tempArray = inputLine.split("&");
+        			
+        				String tempString = tempArray[0].replace(",","");
+        				
+        				value = Double.parseDouble(tempString);
+
+            			break;
+            		}
+        		}
+        	}
+        }
+        in.close();
+		return value;
+	}
+	
+	/**
+	 * NON COMPLETE METHOD.
+	 * 
+	 * @param symbol
+	 * @param key
+	 * @return
+	 * @throws IOException
+	 */
+	public static double generalParser(String symbol, String key) throws IOException{
+		
+        URL oracle = new URL("http://finance.yahoo.com/q/ks?s=" + symbol);
+        URLConnection yc = oracle.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                                    yc.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null){
+        	if(inputLine.contains(key)){
+        		
+        		String line = inputLine;
+        		
+        		int index = line.indexOf(key);
+        		
+        		String[] temp = line.split(key);
+        		
+        		String[] temp2 = temp[1].split("yfnc_tabledata1\">");
+        		
+        		String[] temp3 = temp2[1].split("<");
+        		
+        		String nonparsedValue = temp3[0];
+        	}
+        }
+        in.close();
+		return value;
+	}
+}
