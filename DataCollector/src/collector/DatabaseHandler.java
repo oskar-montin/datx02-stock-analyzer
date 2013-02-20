b086759cb641eed0f8a76a093db8e0a76865e4d9
runaa <runa.gulliksson@gmail.com>
2013-02-20 15:33:39 +0100

    Updated DatabaseHandler and getter for Data-classes

--- DataCollector/src/collector/DatabaseHandler.java
+++ DataCollector/src/collector/DatabaseHandler.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 import data.DailyData;
 import data.QuarterlyData;
@@ -38,7 +39,7 @@ public class DatabaseHandler {
 	
 	public static boolean addStock(Stock stock){
 		
-		String query = "INSERT INTO Stock (symbol, name, stockexchange, business) VALUES('sym', 'name', 'stockexchange', 'business')";
+		String query = "INSERT INTO Stock (symbol, name, stockexchange, business) VALUES('"+stock.getSymbol()+"', '"+stock.getName()+"', '"+stock.getStockExchange()+"', '"+stock.getBusiness()+"')";
 		
 		try {
 			con = DriverManager.getConnection(url + userpass);
@@ -70,21 +71,155 @@ public class DatabaseHandler {
 			return true;
 		
 	}
+
 	
 	public boolean addRTEntry(RTData entry){
-
+		String date = null;
+		String time = null;
+		
+		String queryTime = "INSERT INTO Stock (symbol, date, time, price, orderBook) VALUES('"+time+"')";
+		String queryData = "INSERT INTO Stock (symbol, date, time, price, orderBook) VALUES('"+entry.getSymbol()+"', '"+date+"', '"+time+"', '"+entry.getPrice()+"', '"+entry.getOrderBook()+"')";
+		
+		try {
+			con = DriverManager.getConnection(url + userpass);
+		     st = con.createStatement();
+	         st.executeUpdate(queryData);
+	         st.executeUpdate(queryTime);
+			
+			} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+			return false;
+			} finally {
+	            try {
+	                if (rs != null) {
+	                    rs.close();
+	                }
+	                if (st != null) {
+	                    st.close();
+	                }
+	                if (con != null) {
+	                    con.close();
+	                }} catch (SQLException ex) {
+	                    return false;
+	                }
+	            }
+			
 		return true;
+		
 	}
 	
 	public boolean addDailyData(DailyData data){
 
-		return true;
+		String date = null;
+		String queryValues = "INSERT INTO Stock (stock, date, closePrice, high, low, volume) VALUES('"+data.getSymbol()+"', '"+date+"', '"+data.getClosePrice()+"', '"+data.getHigh()+"', '"+data.getLow()+"', '"+data.getVolume()+"')";
+		String queryKeys = "INSERT INTO Stock (stock, date, marketCap, PE, PS, PEG, dividentYield) VALUES('"+data.getSymbol()+"', '"+date+"', '"+data.getMarketCap()+"', '"+data.getPE()+"', '"+data.getPS()+"', '"+data.getPEG()+"', '"+data.getDividentYield()+"')";
+
+		
+		try {
+			con = DriverManager.getConnection(url + userpass);
+		     st = con.createStatement();
+	         st.executeUpdate(queryValues);
+	         st.executeUpdate(queryKeys);
+
+			
+			} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+			return false;
+			} finally {
+	            try {
+	                if (rs != null) {
+	                    rs.close();
+	                }
+	                if (st != null) {
+	                    st.close();
+	                }
+	                if (con != null) {
+	                    con.close();
+	                }} catch (SQLException ex) {
+	                    return false;
+	                }
+	            }
+			
+			return true;
 	}
 	
 	public boolean addQuarterlyData(QuarterlyData data){
 
-		return true;
+		String date = null;
+		String query = "INSERT INTO Stock (stock, releaseDate, yield, solidity, NAV, dividentPerShare) VALUES('"+data.getSymbol()+"', '"+date+"', '"+data.getYield()+"', '"+data.getSolidity()+"', '"+data.getNAV()+"', '"+data.getDividentPerShare()+"')";
+		
+		try {
+			con = DriverManager.getConnection(url + userpass);
+		     st = con.createStatement();
+	         st.executeUpdate(query);
+
+			
+			} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+			return false;
+			} finally {
+	            try {
+	                if (rs != null) {
+	                    rs.close();
+	                }
+	                if (st != null) {
+	                    st.close();
+	                }
+	                if (con != null) {
+	                    con.close();
+	                }} catch (SQLException ex) {
+	                    return false;
+	                }
+	            }
+			
+			return true;
 	}
+	
+	public boolean addDate(Date date){
+			
+		int year = 0,month = 0,dag = 0 ;
+			
+		String query = "INSERT INTO Date (date) VALUES('DATE: Manual Date', '"+year+"-"+month+"-"+dag+"')";
+		
+		try {
+			con = DriverManager.getConnection(url + userpass);
+		     st = con.createStatement();
+	         st.executeUpdate(query);
+
+			
+			} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+			return false;
+			} finally {
+	            try {
+	                if (rs != null) {
+	                    rs.close();
+	                }
+	                if (st != null) {
+	                    st.close();
+	                }
+	                if (con != null) {
+	                    con.close();
+	                }} catch (SQLException ex) {
+	                    return false;
+	                }
+	            }
+			
+			return true;
+	}
+	
 	
 	
 }
