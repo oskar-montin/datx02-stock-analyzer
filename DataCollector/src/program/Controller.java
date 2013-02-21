dd3859c93551f7c6421f2a1b8e86cb1189356378
oskar-montin <oskar.montin@gmail.com>
2013-02-21 15:08:37 +0100

    Added some javadoc and implemented the rest of the methods.

--- DataCollector/src/program/Controller.java
+++ DataCollector/src/program/Controller.java
@@ -1,14 +1,20 @@
 package program;
 
-import java.io.IOException;
 import java.util.Calendar;
 
 import collector.DatabaseHandler;
 import collector.YahooInterface;
 import data.DailyData;
+import data.QuarterlyData;
+import data.RTData;
 import data.Settings;
 import data.Stock;
 
+/**
+ * 
+ * @author oskar-montin
+ *
+ */
 public class Controller {
 	private Settings settings;
 	private static Controller instance;
@@ -21,6 +27,10 @@ public class Controller {
 		this.controllerProgress = new ProgressInfo();
 	}
 	
+	/**
+	 * 
+	 * @return the only instance of the controller, if no controller exist create a new one.
+	 */
 	public static Controller getInstance() {
 		if(instance == null) {
 			instance = new Controller();
@@ -28,10 +38,18 @@ public class Controller {
 		return instance;
 	}
 	
+	/**
+	 * 
+	 * @return the progress info object representing the progress of the methods in this class
+	 */
 	public ProgressInfo getControllerProgress() {
 		return this.controllerProgress;
 	}
 	
+	/**
+	 * 
+	 * @return the progress info object representing the progress of the real time collector
+	 */
 	public ProgressInfo getRealTimeProgress() {
 		return this.realTimeThread.getProgress();
 	}
@@ -52,62 +70,119 @@ public class Controller {
 		return settings.addSymbol(stock.getSymbol());
 	}
 	
+	/**
+	 * Removes the given symbol from the settings file. This will mean that the stock represented by the 
+	 * symbol will no longer be collected data from. However, no previous data collected from the stock will 
+	 * be removed from the database.
+	 * @param symbol th symbol to remove
+	 * @return true if the symbol was cotained in the settingsfile, false if it didn't exist.
+	 */
 	public boolean removeSymbol(String symbol) {
 		return settings.removeSymbol(symbol);
 	}
 	
+	/**
+	 * Saves all settings to the file system
+	 * @return true if successful
+	 */
 	public boolean saveSettings() {
 		return settings.save();
 	}
 	
+	/**
+	 * Changed the wait time between real time collects.
+	 * @param waitTime the new wait time
+	 */
 	public void changeWaitTime(long waitTime) {
 		settings.changeWaitTime(waitTime);
 	}
 	
+	/**
+	 * Collects all daily data from all symbols in the settings file then adds the data to the database.
+	 */
 	public void collectDailyData() {
 		this.controllerProgress.update(0, "Starting to collect daily data");
 		DatabaseHandler dbh = DatabaseHandler.getInstance();
 		int size = this.settings.getSymbols().size();
 		int i = 0;
 		for(String symbol : this.settings.getSymbols()) {
-			//DailyData data = YahooInterface.getDailyData(symbol);
-			//dbh.addDailyData(data);
+			DailyData data = YahooInterface.getDailyData(symbol);
+			dbh.addDailyData(data);
 			this.controllerProgress.update((i/size)*100, "Data added: "+symbol);
 		}
+		this.controllerProgress.update(100, "Finished. All daily data collected");
 	}
 	
+	/**
+	 * Collects all quarterly data from all symbols in the settings file then adds the data to the database.
+	 */
 	public void collectQuarterlyData() {
-		
+		this.controllerProgress.update(0, "Starting to collect quarterly data");
+		DatabaseHandler dbh = DatabaseHandler.getInstance();
+		int size = this.settings.getSymbols().size();
+		int i = 0;
+		for(String symbol : this.settings.getSymbols()) {
+			QuarterlyData data = YahooInterface.getQuarterlyData(symbol);
+			dbh.addQuarterlyData(data);
+			this.controllerProgress.update((i/size)*100, "Data added: "+symbol);
+		}
+		this.controllerProgress.update(100, "Finished. All quarterly data collected");
 	}
 	
+	/**
+	 * Collects all real time data from all symbols in the settings file then adds the data to the database.
+	 */
 	public void collectRealTimeData() {
-		
+		this.controllerProgress.update(0, "Starting to collect real time data");
+		DatabaseHandler dbh = DatabaseHandler.getInstance();
+		int size = this.settings.getSymbols().size();
+		int i = 0;
+		for(String symbol : this.settings.getSymbols()) {
+			RTData data = YahooInterface.getRTData(symbol);
+			dbh.addRTEntry(data);
+			this.controllerProgress.update((i/size)*100, "Data added: "+symbol);
+		}
+		this.controllerProgress.update(100, "Finished. All real time data collected");
 	}
 	
+	/**
+	 * Starts the real time thread to collect data.
+	 */
 	public void startRealTimeCollecting() {
 		this.realTimeThread.start();
 	}
 	
+	/**
+	 * Stops the real time thread.
+	 */
+	public void stopRealTimeCollecting() {
+		this.realTimeThread.stopRT();
+	}
+	
 	private class RealTimeThread extends Thread{
 		private final ProgressInfo progress;
+		private boolean running;
 		
 		public RealTimeThread() {
 			super();
 			this.progress = new ProgressInfo();
 		}
 		
+		public void stopRT() {
+			this.running = false;
+			this.progress.update(-1, "Stopped");
+		}
+		
 		@Override
 		public void run() {
 			long startTime, endTime,timeDiff;
-			while(true) {
-				this.progress.setValue(0);
-				this.progress.setMessage("Updating realtime stock info");
+			this.running = true;
+			while(this.running) {
+				this.progress.update(0, "Updating realtime stock info");
 				startTime = System.currentTimeMillis();
 				
 				collectRealTimeData();
-				
-				this.progress.setValue(100);
-				this.progress.setMessage("All stocks updated for time: "+
+				this.progress.update(100, "All stocks updated for time: "+
 												Calendar.getInstance().get(Calendar.HOUR)+":"+
 												Calendar.getInstance().get(Calendar.MINUTE));
 				endTime = System.currentTimeMillis();
