3538006cd0c5bb05ae6560a7d40d117de0ae30b1
oskar-montin <oskar.montin@gmail.com>
2013-03-18 17:25:50 +0100

    Changed name from RateOfChange to ROCHandler since the actual rate of change is only a date, a stock and a percent rate. This should be an own class.

--- analyzer/src/analyzer/ROCHandler.java
+++ analyzer/src/analyzer/ROCHandler.java
@@ -1,12 +1,28 @@
 package analyzer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.PriorityQueue;
 
 import data.DailyData;
 
-public class RateOfChange {
+public class ROCHandler {
+	
+	private DailyData[] dailyData;
+	private Double[] rocList;
+	private Double[] maxScope = new Double[2];
+	private Double[] midScope = new Double[2];
+	private Double[] normalScope = new Double[2];
+	private int period;
+	
+	public ROCHandler(PriorityQueue<DailyData> dailyData, int period) {
+		this.dailyData = new DailyData[dailyData.size()];
+		this.dailyData = dailyData.toArray(this.dailyData);
+		
+		this.period = period;
+		
+	}
 	
 	public static Double getROC(PriorityQueue<DailyData> dailyData, Calendar t, int period) {
 		return getROC(dailyData,new DailyData(null, t, null, 0, 0, 0, 0, 0, 0, 0, 0, 0),period);
@@ -20,7 +36,5 @@ public class RateOfChange {
 			return null;
 		}
 		return 100*(temp[tIndex].getClosePrice()-temp[tIndex-period].getClosePrice())/temp[tIndex-period].getClosePrice();
-	}
-	
-	
+	}	
 }
