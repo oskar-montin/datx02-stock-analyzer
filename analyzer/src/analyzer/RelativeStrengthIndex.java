49985c807535932ef20240b1d592c99d42c1ecc5
runaa <runa.gulliksson@gmail.com>
2013-03-20 16:42:31 +0100

    Some methods added and testing done.

--- analyzer/src/analyzer/RelativeStrengthIndex.java
+++ analyzer/src/analyzer/RelativeStrengthIndex.java
@@ -6,17 +6,43 @@ import java.util.PriorityQueue;
 import data.DailyData;
 import data.Stock;
 
+/**
+ * A class that calculates RSI-values.
+ * 
+ * @author Runa Gulliksson
+ */
+
 public class RelativeStrengthIndex {
 
 	private PriorityQueue<DailyData> dailyDataQueue;
-	private LinkedList<Double> RSI;
+	private LinkedList<Double> RSI; // oldest first
 	private double avgGain, avgLoss;
+	private String stock;
 	
-	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		-----For testing-----
+//		RelativeStrengthIndex rsi = new RelativeStrengthIndex(DatabaseHandler.getStock("AAPL"), 2 );
+//		LinkedList<Double> rsList = rsi.getRSI();
+//		while (!rsList.isEmpty()){
+//			System.out.println(rsList.removeLast());
+//		}
+	}
+
+	/**
+	 * Constructor that collects data for a specific stock and calculates RSI
+	 * values for the given priodlength. 
+	 * 
+	 * @param stock
+	 * @param offset - The number of days used for the RSI.
+	 */
 	public RelativeStrengthIndex(Stock stock, int offset) {
 		
 		dailyDataQueue = DatabaseHandler.getDailyData(stock);
 		RSI = new LinkedList<Double>();
+		this.stock = stock.getSymbol();
 		double sumGain = 0, sumLoss = 0, diff;
 		DailyData now, priv;
 		
@@ -33,13 +59,13 @@ public class RelativeStrengthIndex {
 					sumGain = sumGain + diff;
 					
 				}
-				else sumLoss = sumLoss + diff;
+				else sumLoss = sumLoss - diff;
 				
 				priv=now;
 			}
 			
-			avgGain = sumGain/14;
-			avgLoss = sumLoss/14;
+			avgGain = sumGain/offset;
+			avgLoss = sumLoss/offset;
 			RSI.add(100-100/(1+avgGain/avgLoss));
 			
 			while(!dailyDataQueue.isEmpty()){
@@ -48,16 +74,17 @@ public class RelativeStrengthIndex {
 				diff=now.getClosePrice()-priv.getClosePrice();
 				
 				if(diff>0){
-					avgGain=(avgGain*13+diff)/14;
-					avgLoss=(avgLoss*13)/14;
+					avgGain=(avgGain*13+diff)/offset;
+					avgLoss=(avgLoss*13)/offset;
 				}
 				else{
-					avgGain=(avgGain*13)/14;
-					avgLoss=(avgLoss*13+diff)/14;
+					avgGain=(avgGain*13)/offset;
+					avgLoss=(avgLoss*13+diff)/offset;
 					
 				}
 
 				RSI.add(100-100/(1+avgGain/avgLoss));
+
 			}
 
 		}
@@ -65,13 +92,24 @@ public class RelativeStrengthIndex {
 		else System.out.println("error in RSI: to large period.");
 		
 	}
+	
+	/**
+	 * Return RSI-valuelist
+	 * 
+	 * @return Linkedlist of RSI-values oldest first.
+	 */
+	
+	public LinkedList<Double> getRSI(){
+		return RSI;
+	}
 
 	/**
-	 * @param args
+	 * Return stock connected with this instance.
+	 * 
+	 * @return symbol for stock.
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public String getStock(){
+		return stock;
 	}
 
 }
