58ce12bb7bf0637f06134e4891dc35b3a1a16986
oskar-montin <oskar.montin@gmail.com>
2013-03-21 13:44:18 +0100

    Changed the comperators a bit

--- analyzer/src/data/RateOfChange.java
+++ analyzer/src/data/RateOfChange.java
@@ -36,6 +36,26 @@ public class RateOfChange implements Comparable<RateOfChange>{
 		return this.getDate().compareTo(o.getDate());
 	}
 	
+	public static Comparator<RateOfChange> getRateComparator(final boolean abs) {
+		Comparator<RateOfChange> comp = new Comparator<RateOfChange>() {
+
+			@Override
+			public int compare(RateOfChange o1, RateOfChange o2) {
+				if(abs) {
+					Double d1 = Math.abs(o1.getRate());
+					Double d2 = Math.abs(o2.getRate());
+					return d1.compareTo(d2);
+				} else {
+					return compare(o1, o2);
+				}
+				
+				
+			}
+			
+		};
+		return comp;
+	}
+	
 	public static Comparator<RateOfChange> getDateComperator() {
 		Comparator<RateOfChange> comp = new Comparator<RateOfChange>() {
 
