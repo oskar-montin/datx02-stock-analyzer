1f1f815e9d50c0e2dade257c2066537bf7e89fa2
glembo <johanna.glembo@gmail.com>
2013-04-14 23:08:40 +0200

    Ny konstruktor som tar emot en SimpleData-array.

--- analyzer/src/data/Curve.java
+++ analyzer/src/data/Curve.java
@@ -7,6 +7,14 @@ public class Curve {
 	private final PriorityQueue<SimpleData> queue;
 	private final String name;
 	
+	// Constructor with SimpleData array input
+	public Curve(SimpleData[] data, String name) {
+		this.name = name;
+		queue = new PriorityQueue<SimpleData>();
+		for (int i = 0; i < data.length; i++)
+			queue.add(new SimpleData(data[i]));
+	}
+	
 	public Curve(PriorityQueue<SimpleData> queue, String name){
 		this.queue = queue;
 		this.name = name;
