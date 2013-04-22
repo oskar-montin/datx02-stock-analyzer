b816619ed2aba7b5f328da53f30821d7f8e306bd
oskar-montin <oskar.montin@gmail.com>
2013-04-22 19:02:14 +0200

    AnalyticsData implemented, not tested

---
+++ analyzer/src/data/AnalyticsData.java
@@ -0,0 +1,68 @@
+package data;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class AnalyticsData {
+	private final Stock stock;
+	private final String analysisMethod;
+	private ArrayList<BuySellPair> pairs;
+	private final double successRate;
+	private int timesSucceeded = 0;
+	private int timesFailed = 0;
+	private double totalProfit = 0;
+	private final int amountStillOwned;
+	
+	public AnalyticsData(PriorityQueue<SimpleData> bought, PriorityQueue<SimpleData> sold, String analysisMethod, Stock stock) {
+		this.stock = stock;
+		this.analysisMethod = analysisMethod;
+		pairs = new ArrayList<BuySellPair>();
+		PriorityQueue<SimpleData> buyQueue = new PriorityQueue<SimpleData>(bought);
+		for(SimpleData sell:sold) {
+			SimpleData buy = buyQueue.poll();
+			BuySellPair pair = new BuySellPair(buy,sell);
+			pairs.add(pair);
+			if(pair.succeeded()) {
+				timesSucceeded++;
+			} else {
+				timesFailed++;
+			}
+			totalProfit+=pair.profit();
+		}
+		this.amountStillOwned = buyQueue.size();
+		this.successRate = this.timesSucceeded/sold.size();
+		
+	}
+
+	public Stock getStock() {
+		return stock;
+	}
+
+	public String getAnalysisMethod() {
+		return analysisMethod;
+	}
+
+	public double getSuccessRate() {
+		return successRate;
+	}
+	
+	public ArrayList<BuySellPair> getPairs() {
+		return pairs;
+	}
+
+	public int getTimesSucceeded() {
+		return timesSucceeded;
+	}
+
+	public int getTimesFailed() {
+		return timesFailed;
+	}
+
+	public double getTotalProfit() {
+		return totalProfit;
+	}
+
+	public int getAmountStillOwned() {
+		return amountStillOwned;
+	}
+}
