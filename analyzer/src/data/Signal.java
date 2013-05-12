345b88ec710df1940e1a12a2e7d12aa6a10df531
oskar-montin <oskar.montin@gmail.com>
2013-05-13 01:12:49 +0200

    Values is now -1, 0 and 1 in signal

--- analyzer/src/data/Signal.java
+++ analyzer/src/data/Signal.java
@@ -1,12 +1,17 @@
 package data;
 
 public enum Signal {
-	BUY("Buy"), SELL("Sell"), NONE("Keep");
+	BUY("Buy",1), SELL("Sell",-1), NONE("Keep",0);
 	private String string;
-	Signal(String string) {
+	private int value;
+	Signal(String string, int value) {
 		this.string = string;
+		this.value = value;
 	}
 	public String getString() {
 		return this.string;
+	}
+	public int getValue() {
+		return this.value;
 	}
 }
