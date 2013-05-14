db33bc3b994079fa4c35568bdd9ea36091781f02
oskar-montin <oskar.montin@gmail.com>
2013-05-15 01:08:59 +0200

    Everything but the actual buying and selling seems to work in testbot

--- analyzer/src/controller/TestBot.java
+++ analyzer/src/controller/TestBot.java
@@ -1,16 +1,20 @@
 package controller;
 
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.SortedMap;
 import java.util.Map.Entry;
 import java.util.PriorityQueue;
 import java.util.TreeMap;
 
 import util.IO;
+import util.Util;
 
 import analyzer.AnalysisMethod;
 import analyzer.RateOfChange;
@@ -34,7 +38,7 @@ public class TestBot {
 	private final int endDate;
 	private int currentDate;
 	private ArrayList<DailyData>[] data;
-	private TreeMap<Double,String> safetyMap;
+	private PriorityQueue <Entry<String,Double>> safetyQueue;
 	
 	public TestBot(int startDate, int testLength) {
 		index = new User("Index");
@@ -48,16 +52,36 @@ public class TestBot {
 	}
 	
 	private void init() {
-		safetyMap = (TreeMap<Double,String>) IO.loadFromFile("MethodSafeties.dat");
+		TreeMap<String,Double> safetyMap = (TreeMap<String,Double>) IO.loadFromFile("MethodSafeties.dat");
+		Comparator<Entry<String,Double>> comp = Util.entryValueComparator();
+		safetyQueue = new PriorityQueue<Entry<String,Double>>(safetyMap.size(),comp);
+		for(Entry<String,Double> e:safetyMap.entrySet()) {
+			safetyQueue.add(e);
+		}
+		
 		PriorityQueue<Stock> temp = DatabaseHandler.getAllStocks();
 		stocks = new Stock[temp.size()];
-		stocks = temp.toArray(stocks);
+		int i = 0;
+		for(Stock s:temp) {
+			System.out.println(s.getSymbol());
+			stocks[i] = new Stock(s.getName(), s.getSymbol(), s.getBusiness(), s.getStockExchange());
+			i++;
+		}
 		data = new ArrayList[stocks.length];
 		dates = new ArrayList<Calendar>(DatabaseHandler.getDates());
-		int i = 0;
+		i=0;
 		for(Stock s:stocks) {
-			data[i] = new ArrayList<DailyData>(DatabaseHandler.getDailyData(s, dates.get(startDate), dates.get(endDate)));
-			index.performTransaction(new Transaction(new SimpleData(data[i].get(currentDate-1)), 100));
+			ArrayList<DailyData> a = new ArrayList<DailyData>(DatabaseHandler.getDailyData(s, dates.get(0), dates.get(dates.size()-1)));
+			PriorityQueue<DailyData> dd = DatabaseHandler.getDailyData(s, dates.get(startDate), dates.get(endDate));
+			data[i] = new ArrayList<DailyData>(dd);
+			if(data[i].size()==endDate-startDate) {
+				SimpleData firstDay = new SimpleData(data[i].get(currentDate-1));
+				index.deposit(firstDay.getValue()*100);
+				index.performTransaction(new Transaction(firstDay, 100));
+			} else {
+				data[i] = new ArrayList<DailyData>();
+			}
+			i++;
 		}
 		user.deposit(index.getTotalAssets());
 		indexList.add(new SimpleData(stocks[0], dates.get(startDate+currentDate-1), index.getTotalAssets()));
@@ -68,9 +92,14 @@ public class TestBot {
 		double [] stockValues = new double[stocks.length];
 		String userCsv = ""+user.getTotalAssets();
 		String indexCsv = ""+index.getTotalAssets();
-		while(currentDate<data[0].size()) {
+		while(currentDate<endDate-startDate) {
 			for(int i = 0; i<data.length;i++) {
-				stockValues[i] = TA((ArrayList<DailyData>) data[i].subList(currentDate-42, currentDate));
+				//System.out.println(data[i].get(0).getSymbol());
+				if(data[i].size()==endDate-startDate) {
+					stockValues[i] = TA(new ArrayList<DailyData>(data[i].subList(currentDate-42, currentDate)));
+				} else {
+					stockValues[i] = 0;
+				}
 			}
 			ArrayList<Transaction> portfolio = portfolioConstructor(stockValues);
 			for(Transaction t:portfolio) {
@@ -85,15 +114,22 @@ public class TestBot {
 	}
 	
 	private ArrayList<Transaction> portfolioConstructor(double[] stockValues) {
-		TreeMap<Double, SimpleData> buyCandidates = new TreeMap<Double,SimpleData>();
+		TreeMap<SimpleData,Double> buyCandidates = new TreeMap<SimpleData,Double>();
 		ArrayList<Transaction> transactions = new ArrayList<Transaction>();
 		double balance = user.getBalance();
-		
+		SimpleData today = null;
 		for(int i = 0;i<stockValues.length;i++) {
-			SimpleData today = data[i].get(data[i].size()-1);
-			if(stockValues[i]>0.5) {
-				buyCandidates.put(stockValues[i],today);
+			DailyData[] dailyData = new DailyData[data[i].size()];
+			dailyData = data[i].toArray(dailyData);
+			if(stockValues[i]>0.2) {
+				System.out.println(dailyData[currentDate].equals(today));
+				System.out.println(data[i].get(currentDate).getStock().getSymbol());
+				//today = new DailyData(dailyData[currentDate]);
+				//today = data[i].get(currentDate);
+				//buyCandidates.put(today,stockValues[i]);
+				buyCandidates.put(new SimpleData(data[i].get(currentDate)),stockValues[i]);
 			} else if(stockValues[i]<0) {
+				today = data[i].get(currentDate);
 				if(user.ownStock(today.getStock())) {
 					int amount = user.amountOfStocks(today.getStock());
 					user.performTransaction(new Transaction(today,-amount));
@@ -113,29 +149,30 @@ public class TestBot {
 		return transactions;
 	}
 	
-	private TreeMap<SimpleData,Integer> distribute(TreeMap<Double, SimpleData> buyCandidates, double balance, int maxCount) {
-		Entry<Double,SimpleData> entry;
-		TreeMap<Double, SimpleData> choosenEntries = new TreeMap<Double, SimpleData>();
+	private TreeMap<SimpleData,Integer> distribute(TreeMap<SimpleData,Double> buyCandidates, double balance, int maxCount) {
+		Entry<SimpleData,Double> entry;
+		TreeMap<SimpleData,Double> choosenEntries = new TreeMap<SimpleData,Double>();
 		TreeMap<SimpleData,Integer> distribution = new TreeMap<SimpleData,Integer>();
 		int i = 0;
 		double sum = 0;
 		double usedBalance = 0;
-		entry = buyCandidates.pollLastEntry();
 		maxCount = Math.min(buyCandidates.size(), maxCount);
+		entry = buyCandidates.pollLastEntry();
+		
 		//Choose entries:
 		while(entry !=null && i<maxCount) {
 			choosenEntries.put(entry.getKey(), entry.getValue());
-			sum+=entry.getKey();
+			sum+=entry.getValue();
 			i++;
 			entry = buyCandidates.pollLastEntry();
 		}
 		
 		//Calculate shares and add transactions
-		for(Entry<Double, SimpleData> e: choosenEntries.entrySet()) {
-			double share = balance*e.getKey()/sum;
-			int amount = (int) (share/e.getValue().getValue());
-			distribution.put(e.getValue(), amount);
-			usedBalance += amount*e.getValue().getValue();
+		for(Entry<SimpleData,Double> e: choosenEntries.entrySet()) {
+			double share = balance*e.getValue()/sum;
+			int amount = (int) (share/e.getKey().getValue());
+			distribution.put(e.getKey(), amount);
+			usedBalance += amount*e.getKey().getValue();
 		}
 		
 		return distribution;
@@ -143,13 +180,13 @@ public class TestBot {
 	}
 
 	private double TA(ArrayList<DailyData> data) {
-		TreeMap<Double, String> tempMap = new TreeMap<Double, String>(safetyMap);
-		if(data != null && data.size()>42) {
+		
+		if(data != null && data.size()>=42) {
 			ArrayList<AnalysisMethod> methods = getMethods(data);
-			for(Entry<Double,String> entry = tempMap.pollLastEntry(); entry != null; entry = tempMap.pollLastEntry()) {
+			for(Entry<String,Double> entry:safetyQueue) {
 				for(AnalysisMethod am:methods) {
-					if(am.getName().equals(entry.getValue())) {
-						double res = am.getSignal().getValue()*entry.getKey();
+					if(am.getName().equals(entry.getKey())) {
+						double res = am.getSignal().getValue()*entry.getValue();
 						if(res!=0) {
 							return res;
 						}
@@ -167,6 +204,12 @@ public class TestBot {
 		return methods;
 	}
 
+	public static void main(String[] args) {
+		ArrayList<Calendar> dates = new ArrayList<Calendar>(DatabaseHandler.getDates());
+		TestBot bot = new TestBot(45, 338);
+		bot.run();
+		System.out.println(bot.user.getTotalAssets());
+	}
 	
 	
 }
