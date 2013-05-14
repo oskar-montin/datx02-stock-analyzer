cbbe3e83ada036c0c46f789026d37eced0808dd0
oskar-montin <oskar.montin@gmail.com>
2013-05-14 12:41:42 +0200

    IO: added method for simple text to file writing

--- analyzer/src/util/IO.java
+++ analyzer/src/util/IO.java
@@ -2,6 +2,7 @@ package util;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -16,6 +17,43 @@ import java.io.Serializable;
 import controller.Settings;
 
 public class IO {
+	
+	public static void writeToFile(String content, String fileName) {
+		FileOutputStream fop = null;
+		File file;
+ 
+		try {
+ 
+			file = new File(fileName);
+			fop = new FileOutputStream(file);
+ 
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+ 
+			// get the content in bytes
+			byte[] contentInBytes = content.getBytes();
+ 
+			fop.write(contentInBytes);
+			fop.flush();
+			fop.close();
+ 
+			System.out.println("Wrote to file: "+fileName);
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fop != null) {
+					fop.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	/**
 	 * Saves the object to the file with the filename
 	 * @return true if successful
